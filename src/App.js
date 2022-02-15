90468828bef224edccd375807669537acee2412b
danish <danishgupta1207@gmail.com>
2022-02-15 16:54:57 +0530

    da

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+ 
+import './App.css';
+import ToDo from './ToDo';
+ 
+function App() {
+ return (
+   <div className='mainb'>
+   <ToDo/>
+     </div>
+ );
+}
+ 
+export default App;
